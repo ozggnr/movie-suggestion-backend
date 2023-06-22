e9e9729704caf90acaf1b7f9a8fbf4567ba30fbb
ozge <ozgecaylioglu@gmail.com>
2023-06-21 22:01:42 -0400

    migrate from previous repo

---
+++ movie_server/user_auth/urls.py
@@ -0,0 +1,14 @@
+# from rest_framework.routers import DefaultRouter
+# from django.urls import path, include
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+#     TokenRefreshView,
+# )
+
+# router = DefaultRouter
+# router.register(r'token', TokenObtainPairView.as_view())
+# router.register(r'token/refresh', TokenRefreshView.as_view())
+
+# urlpatterns = [
+#     path('auth/', include(router.urls))
+# ]
